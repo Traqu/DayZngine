d0b4b275f5216ed269043d6104356840dc587f42
Traqu <piotr.smolik1998@gmail.com>
2024-07-25 03:39:11 +0200

    Add Lombok library

--- src/main/java/traqu/mvc/view/MainView.java
+++ src/main/java/traqu/mvc/view/MainView.java
@@ -1,10 +1,12 @@
 package traqu.mvc.view;
 
+import lombok.Getter;
 import traqu.mvc.controller.MainViewController;
 import traqu.mvc.view.viewbase.View;
 
 import javax.swing.*;
 
+@Getter
 public class MainView extends View {
     private JPanel mainPanel;
     private JPanel infoPanel;
@@ -21,54 +23,6 @@ public class MainView extends View {
 
     public MainView() {
         initializeView(mainPanel);
-    }
-
-    public JPanel getMainPanel() {
-        return mainPanel;
-    }
-
-    public JPanel getInfoPanel() {
-        return infoPanel;
-    }
-
-    public JPanel getSettingsPanel() {
-        return settingsPanel;
-    }
-
-    public JPanel getToolsPanel() {
-        return toolsPanel;
-    }
-
-    public JButton getCrackButton() {
-        return crackButton;
-    }
-
-    public JTextField getSecondsAmtInput() {
-        return secondsAmtInput;
-    }
-
-    public JButton getLanguageButton() {
-        return languageButton;
-    }
-
-    public JTextField getCirclesAmtInput() {
-        return circlesAmtInput;
-    }
-
-    public JProgressBar getProgressBar1() {
-        return progressBar1;
-    }
-
-    public JButton getButton1() {
-        return button1;
-    }
-
-    public JButton getPresetsButton() {
-        return presetsButton;
-    }
-
-    public JComboBox<String> getPresetsCombobox() {
-        return presetsCombobox;
     }
 
     @Override
